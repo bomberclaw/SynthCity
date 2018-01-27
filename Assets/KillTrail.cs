4fe7482c6c7f7659272a87e884e406a5bc46287f
DESKTOP-U8BRHG3\Matias <matiashollmann@hotmail.com.ar>
2018-01-27 17:20:59 -0300

    Push project settings, linear an deferred.

---
+++ Assets/KillTrail.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillTrail : MonoBehaviour {
+
+    public TrailRenderer[] trails;
+
+
+
+    protected virtual void OnEnable()
+    {
+
+
+        for (int i = 0; i < trails.Length; i++)        // Si tiene trails asignados, los resetea (es importante que la particula se prenda siempre despues de posicionarse)
+        {
+            trails[i].Clear();
+            trails[i].enabled = true;
+        }
+     
+    }
+
+    protected void OnDisable()
+    {
+        
+        for(int i = 0; i < trails.Length; i++) {
+            trails[i].Clear();
+            trails[i].enabled = false;
+
+        }
+    }
+}
